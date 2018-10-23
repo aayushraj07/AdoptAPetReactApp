f8239b4a6fb3534add12d73e882f48f48ad84a76
aayushraj07 <aayush.97raj@gmail.com>
2018-10-23 21:09:49 +0530

    router initialized

--- src/App.js
+++ src/App.js
@@ -1,68 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import pf from "petfinder-client";
-import Pet from "./Pet";
+import Results from "./Results";
 
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-});
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pets: []
-    };
-  }
-
-  componentDidMount() {
-    petfinder.pet
-      .find({ output: "full", location: "Seattle, WA" })
-      .then(data => {
-        let pets;
-
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          //this conditon checks whether it has pets or not.
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            //this condition checks that  if the gathered data from API is in array or not
-            pets = data.petfinder.pets.pet;
-          } else {
-            pets = [data.petfinder.pets.pet]; //if the data is not in array then it will make one , array
-          }
-        } else {
-          pets = []; //else will remain as it is , empty
-        }
-        this.setState({ pets });
-        // console.log(data);
-      });
-  }
+class App extends React.Componet {
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <div>
-          {this.state.pets.map(pet => {
-            let breed;
-
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(", ");
-            } else {
-              breed = pet.breeds.breed;
-            }
-            return (
-              <Pet
-                key={pet.id}
-                animal={pet.animal}
-                name={pet.name}
-                breed={breed}
-                media={pet.media}
-                location={`${pet.contact.city},${pet.contact.state}`}
-              />
-            );
-          })}
-        </div>
+        <Results />
       </div>
     );
   }
---
+++ src/Results.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+import pf from "petfinder-client";
+import Pet from "./Pet";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Seattle, WA" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          //this conditon checks whether it has pets or not.
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            //this condition checks that  if the gathered data from API is in array or not
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet]; //if the data is not in array then it will make one , array
+          }
+        } else {
+          pets = []; //else will remain as it is , empty
+        }
+        this.setState({ pets });
+        // console.log(data);
+      });
+  }
+  render() {
+    return (
+      <div className="search">
+        {this.state.pets.map(pet => {
+          let breed;
+
+          if (Array.isArray(pet.breeds.breed)) {
+            breed = pet.breeds.breed.join(", ");
+          } else {
+            breed = pet.breeds.breed;
+          }
+          return (
+            <Pet
+              key={pet.id}
+              animal={pet.animal}
+              name={pet.name}
+              breed={breed}
+              media={pet.media}
+              location={`${pet.contact.city},${pet.contact.state}`}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Results;
